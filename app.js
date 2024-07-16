8a3731814fd258dc6be90b10a85ef5d192d2f3e2
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-16 12:02:52 -0300

    Funcao que altera o texto e gera numero aleatorio

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+function alteraTexto(tag, texto) {
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto
+}
 
-let textoParagrafo = document.getElementsByClassName('texto__paragrafo')
-textoParagrafo[0].innerHTML = 'Escolha um número entre 1 e 10';
+alteraTexto('h1', 'Jogo do número secreto');
+alteraTexto('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute() {
-    
+function geraNumeroAleatorio(range) {
+    let numeroAleatorio = parseInt(Math.random() * range + 1);
+    return numeroAleatorio;
 }
