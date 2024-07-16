32c72fec4ec6bb5c0026661a42b9fefe4bc067e6
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-16 11:48:19 -0300

    Projeto criado

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let textoParagrafo = document.getElementsByClassName('texto__paragrafo')
+textoParagrafo[0].innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    
+}
